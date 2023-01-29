234a3d177171d7240c7f3043fc575025a57b8e46
SaketCSIT <81370601+SaketCSIT@users.noreply.github.com>
2023-01-29 17:10:23 +0530

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,13 @@
+#include<conio.h>
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a;
+    for (a=1;a<100; a++)
+    {
+       printf("Hello World\n");
+    }
+    
+
+    return 0;
+}
